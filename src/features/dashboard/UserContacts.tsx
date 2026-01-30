81e35dac81c8d52bd393a818163b1626f7497548
Денис <denis@MacBook-Air-Oleksandra.local>
2026-01-30 22:19:46 +0200

    work with deshboard adptation

--- src/features/dashboard/UserContacts.tsx
+++ src/features/dashboard/UserContacts.tsx
@@ -5,8 +5,8 @@ export const UserContacts = () => {
   const user = useUserStore((state) => state.currentUser);
 
   return (
-    <div className="text-[20px] flex items-center gap-5 pl-2 md:pl-0 w-full mb-5 leading-[130%]">
-      <UserIcon classname="text-[#5B242A] text-20px" />
+    <div className="text-[20px] flex items-center gap-5 md:gap-3 pl-2 md:pl-0 w-full mb-5 md:mb-8 leading-[130%]">
+      <UserIcon classname="text-[#5B242A]" />
 
       <div className="flex flex-col items-start justify-center gap-3 ">
         <div className="text-[#5B242A] font-medium ">
--- src/features/dashboard/UserOrders.tsx
+++ src/features/dashboard/UserOrders.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from "react";
 import { useOrderStore } from "@/store";
+import { useWindowWidth } from "@/lib/hooks/useWindowWidth";
 
 export const UserOrders = () => {
 
   const { orders, fetchOrders } = useOrderStore() 
+
+  const {isMobile} = useWindowWidth()
 
   const dateChanger = (date :string) => {
     const dataObj = new Date(date).toLocaleDateString('uk-UA', {
@@ -33,11 +36,14 @@ const getStatusLabel = (status: string) => {
 
   return (
     <>
+    {isMobile && (
       <h2 className="text-button text-[20px] text-center p-8">Історія Замовлень</h2>
+    )}
+      
       {orders.map((item) => (
-        <div key={item.id} className="container">
-          <div className="flex justify-between items-baseline pb-3">
-            <span className="text-[20px] font-normal">
+        <div key={item.id} className="container md:pl-0 md:pt-10 lg:pt-35">
+          <div className="flex justify-between items-baseline pb-3 md:pb-12">
+            <span className="text-[20px] font-normal md:font-medium">
               Замовлення № {item.orderNumber.slice(-4)}
             </span>
             <span className="text-[16px] text-[#727272]">
@@ -52,20 +58,33 @@ const getStatusLabel = (status: string) => {
           <img 
             src={item.items[0].product.images[0].url} 
             alt="Замовлений товар"
-            className="w-2/5 pt-3"
+            className="w-2/5 pt-3 md:pt-5 lg:pt-7 lg:w-60"
             />
 
-          <div className="flex gap-1 py-1">
-            <span>{item.items[0].product.categoryName}</span>
-            <span>"{item.items[0].product.collectionName}"</span>
+          <div className="flex gap-1 py-1 md:justify-between lg:py-2">
+            <div className="flex gap-1">
+              <span>{item.items[0].product.categoryName}</span>
+              <span>"{item.items[0].product.collectionName}"</span>
+            </div>
+            {!isMobile ? (
+              <span className="text-button text-[16px]">
+                {item.items[0].product.price.normalPrice} грн
+              </span>
+            ): (
+              null
+            )}
           </div>
 
+          {isMobile? (
           <span className="text-button text-[16px]">
             {item.items[0].product.price.normalPrice} грн
           </span>
+          ) : (
+            null
+          )}
+         
 
-          <div className=" flex flex-col gap-4 pt-5 pb-16">
-            
+          <div className=" flex flex-col gap-4 pt-5 pb-8 md:pb-16 lg:gap-5">
               {item.isGift && (  
                 <div className="flex justify-between">
                   <span>Подарункове пакування</span>
@@ -78,14 +97,14 @@ const getStatusLabel = (status: string) => {
               <span className="text-button">0 грн</span>
             </div>
 
-            <div className="flex justify-between text-[16px] font-medium">
+            <div className="flex justify-between text-[16px] font-medium md:text-[20px] ">
               <span>Разом </span>
               <span className="text-button">{item.isGift ? item.totalPrice + 240 + " грн" : item.totalPrice + ' грн'}</span>
             </div>
+          </div >
+          <div className="flex justify-end">
+          <button className="flex justify-center w-full border-1 p-3 text-[16px] mb-15 md:mb-0 md:w-1/2 md:mx-auto lg:mx-0 lg:block lg:w-2/10 lg:justify-end">Залишити відгук</button>
           </div>
-
-        <button className="w-full border-1 p-3 text-[16px] mb-21">Залишити відгук</button>
-          
         </div>
       ))}
     </>
